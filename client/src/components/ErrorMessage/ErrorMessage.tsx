664c9523f08c611e7b7b455de185723b3ee64590
Danny Reyes <reyesdmusic@gmail.com>
2023-03-27 19:36:31 -0400

    Remove unused css file

--- client/src/components/ErrorMessage/ErrorMessage.tsx
+++ client/src/components/ErrorMessage/ErrorMessage.tsx
@@ -3,10 +3,9 @@ import "./ErrorMessage.css";
 function ErrorMessage({ error }) {
   if (!error) return null;
 
-    
   return (
     <div role="alert" className="error-message">
-        Sorry, couldn't find that location
+      Sorry, couldn't find that location
     </div>
   );
 }
