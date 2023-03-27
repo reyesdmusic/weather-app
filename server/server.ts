4248f7dfab3ba0d334e7dc7982cde192a801857e
Danny Reyes <reyesdmusic@gmail.com>
2023-03-27 12:17:02 -0400

    Send 400s on bad request

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,11 @@ const API_KEY = "ff3af498ead27371a1dcb730a1c7e5a7";
 app.get("/api/weather", (req: Request, res: Response) => {
   const { location, lat, lon } = req?.query;
 
-  if (!location && (!lat || !lon)) return;
+  if (!location && (!lat || !lon)) {
+    res.status(400).send();
+    handleError(400);
+    return
+  };
 
   // Set URL to lat and lon when it's provided,
   // if the location is a number set it to search by zip, 
@@ -43,8 +47,14 @@ app.get("/api/weather", (req: Request, res: Response) => {
 app.get("/api/forecast", (req: Request, res: Response) => {
   const { location, lat, lon } = req?.query;
 
-  if (!location && (!lat || !lon)) return;
+  if (!location && (!lat || !lon)) {
+    res.status(400).send();
+    handleError(400);
+    return
+  };
 
+  // Set URL to lat and lon when it's provided,
+  // otherwise set it search by location - which can be name or zip
   let url = lat && lon
     ? `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
     : `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&appid=${API_KEY}`;
@@ -68,7 +78,11 @@ app.get("/api/forecast", (req: Request, res: Response) => {
 app.get("/api/location", (req: Request, res: Response) => {
   const location = req.query.location;
 
-  if (!location) return;
+  if (!location) {
+    res.status(400).send();
+    handleError(400);
+    return
+  };
 
   const GEO_LOCATE_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API_KEY}`;
 
